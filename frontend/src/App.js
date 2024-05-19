37ef32b85e3809caa555fb4fdb1c26cb5d5c96bf
Ankit Rout <88599131+ankitrout2903@users.noreply.github.com>
2024-05-20 01:15:05 +0530

    toast update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,72 +1,77 @@
-import './App.css';
 import { useEffect, useReducer } from 'react';
+import { Toaster } from 'react-hot-toast';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import axios from './Axios/axios.js';
 import Active from './components/Active';
-import Completed from './components/Completed';
 import AllTask from './components/AllTask';
+import Completed from './components/Completed';
+import Header from './components/Header/Header';
 import Layout from './components/Layout';
+import Login from './components/Login';
+import Register from './components/Register';
+import ForgotPassword from './components/forgotPassword/ForgotPassword';
+import ResetPassword from './components/forgotPassword/ResetPassword';
 import TaskContext from './context/TaskContext';
 import TokenContext from './context/TokenContext';
 import taskReducer from './reducer/taskReducer';
 import tokenReducer from './reducer/tokenReducer';
 import userReducer from './reducer/userReducer';
-import Header from './components/Header/Header';
-import Login from './components/Login';
-import Register from './components/Register';
-import ForgotPassword from './components/forgotPassword/ForgotPassword';
-import ResetPassword from './components/forgotPassword/ResetPassword';
-import axios from './Axios/axios.js';
+
 function App() {
-  const token = JSON.parse(localStorage.getItem("authToken"));
-  const [tasks, dispatch] = useReducer(taskReducer, [])
-  const [userToken, tokenDispatch] = useReducer(tokenReducer, token)
-  const [user, userDispatch] = useReducer(userReducer, {})
+  const token = JSON.parse(localStorage.getItem('authToken'));
+  const [tasks, dispatch] = useReducer(taskReducer, []);
+  const [userToken, tokenDispatch] = useReducer(tokenReducer, token);
+  const [user, userDispatch] = useReducer(userReducer, {});
   useEffect(() => {
-    console.log("App.js");
+    console.log('App.js');
     const fetchUser = async () => {
       try {
-        console.log("fetchUser");
-        const res = await axios.get("/user/getUser",{
+        console.log('fetchUser');
+        const res = await axios.get('/user/getUser', {
           headers: {
-            Authorization: `Bearer ${userToken}`
-          }
-        })
+            Authorization: `Bearer ${userToken}`,
+          },
+        });
         //tokenDispatch({type: "SET_TOKEN", payload: res.token})
-        console.log("res.data: ", res.data);
-        userDispatch({type: "SET_USER", payload:res.data.user})
+        console.log('res.data: ', res.data);
+        userDispatch({ type: 'SET_USER', payload: res.data.user });
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     if (userToken) {
-      fetchUser()
+      fetchUser();
     }
-  },[userToken])
+  }, [userToken]);
   useEffect(() => {
     const fetchTasks = async () => {
       try {
-        console.log("fetchTasks");
-        const res = await axios.get("/task/getTask", {
+        console.log('fetchTasks');
+        const res = await axios.get('/task/getTask', {
           headers: {
-            Authorization: `Bearer ${userToken}`
-          }
-        })
-        dispatch({ type: "SET_TASK", payload: res.data })
+            Authorization: `Bearer ${userToken}`,
+          },
+        });
+        dispatch({ type: 'SET_TASK', payload: res.data });
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     if (userToken) {
-      fetchTasks()
+      fetchTasks();
     }
-  },[userToken])
+  }, [userToken]);
   return (
     <BrowserRouter>
-      <TokenContext.Provider value={{userToken, tokenDispatch, user, userDispatch}}>
+      <TokenContext.Provider
+        value={{ userToken, tokenDispatch, user, userDispatch }}
+      >
         <TaskContext.Provider value={{ tasks, dispatch }}>
+          <Toaster />
           <Routes>
             <Route path="/" element={<Header />}>
-              <Route path='/' element={token ? <Layout /> : <Login />}>
+              <Route path="/" element={token ? <Layout /> : <Login />}>
                 <Route index element={<AllTask />} />
                 <Route path="active" element={<Active />} />
                 <Route path="completed" element={<Completed />} />
@@ -79,7 +84,6 @@ function App() {
           </Routes>
         </TaskContext.Provider>
       </TokenContext.Provider>
-
     </BrowserRouter>
   );
 }
--- frontend/src/components/Task/Task.jsx
+++ frontend/src/components/Task/Task.jsx
@@ -1,55 +1,131 @@
-import React from 'react';
+import { Delete, Edit, NotificationImportant } from '@mui/icons-material/';
+import axios from 'axios';
 import moment from 'moment';
-import "./task.css";
-import { useContext } from 'react';
+import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
 import TaskContext from '../../context/TaskContext';
-import DeleteIcon from '@mui/icons-material/Delete';
+import TokenContext from '../../context/TokenContext';
+import './task.css';
+
 function Task({ task, id }) {
-    const { dispatch } = useContext(TaskContext);
+  const { dispatch } = useContext(TaskContext);
+  const userToken = useContext(TokenContext);
 
-    const handleRemove = (e) => {
-        e.preventDefault();
-        
+  const handleRemove = async (e) => {
+    e.preventDefault();
 
-        dispatch({
-            type: "REMOVE_TASK",
-            id
-        })
+    try {
+      await axios.get(
+        '/task/removeTask',
+        {
+          id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
+        }
+      );
+
+      dispatch({
+        type: 'REMOVE_TASK',
+        id,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const handleMarkDone = (e) => {
-        dispatch({
-            type: "MARK_DONE",
-            id
-        })
+  const handleReminder = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post(
+        '/task/reminder',
+        {
+          id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
     }
-    return (
-        <div className='bg-slate-300 py-4 rounded-lg shadow-md flex items-center justify-center gap-2 mb-3'>
-            <div className="mark-done">
-                <input type="checkbox" className="checkbox" onChange={handleMarkDone} checked={task.completed} />
-            </div>
-            <div className="task-info text-slate-900 text-sm w-10/12">
-                <h4 className="task-title text-lg capitalize">{task.title}</h4>
-                <p className="task-description">{task.description}</p>
-                <div className=' italic opacity-60'>
-                    {
-                        task?.createdAt ? (
-                            <p>{moment(task.createdAt).fromNow()}</p>
-                        ) : (
-                            <p>just now</p>
-                        )
-                    }
-                </div>
-            </div>
-            <div className="remove-task text-sm text-white">
-                <DeleteIcon
-                    style={{ fontSize: 30, cursor: "pointer" }}
-                    size="large"
-                    onClick={handleRemove}
-                    className="remove-task-btn bg-blue-700 rounded-full border-2 shadow-2xl border-white p-1" />
-            </div>
+  };
+
+  const handleMarkDone = (e) => {
+    if (task.completed) toast.success('Task marked as incomplete');
+    else toast.success('Task marked as complete');
+
+    dispatch({
+      type: 'MARK_DONE',
+      id,
+    });
+  };
+
+  return (
+    <div className="bg-slate-300 py-4 rounded-lg shadow-md flex items-center justify-center gap-2 mb-3 px-2">
+      <div className="mark-done">
+        <input
+          type="checkbox"
+          className="checkbox h-4 w-4"
+          onChange={handleMarkDone}
+          checked={task.completed}
+        />
+      </div>
+      <div className="task-info text-slate-900 text-sm w-10/12">
+        <h4 className="task-title text-lg capitalize">{task.title}</h4>
+        <p className="task-description">{task.description}</p>
+        <div className="flex justify-between">
+          <div className="italic opacity-60">
+            {task?.createdAt ? (
+              <p>{moment(task.createdAt).fromNow()}</p>
+            ) : (
+              <p>just now</p>
+            )}
+          </div>
+          <div className="italic opacity-60">
+            {task?.dueDate ? (
+              <p>
+                Due: {moment(task.dueDate).format('MMMM DD YYYY, h:mm:ss a')}
+              </p>
+            ) : (
+              <p>No due date</p>
+            )}
+          </div>
         </div>
-    );
+      </div>
+      <div className="flex flex-col gap-1">
+        <div className="remove-task text-sm text-white">
+          <Edit
+            style={{ fontSize: 30, cursor: 'pointer' }}
+            size="large"
+            onClick={handleReminder}
+            className="remove-task-btn bg-blue-700 rounded-full border-2 shadow-2xl border-white p-1"
+          />
+        </div>
+        <div className="remove-task text-sm text-white">
+          <NotificationImportant
+            style={{ fontSize: 30, cursor: 'pointer' }}
+            size="large"
+            onClick={handleReminder}
+            className="remove-task-btn bg-blue-700 rounded-full border-2 shadow-2xl border-white p-1"
+          />
+        </div>
+        <div className="remove-task text-sm text-white">
+          <Delete
+            style={{ fontSize: 30, cursor: 'pointer' }}
+            size="large"
+            onClick={handleRemove}
+            className="remove-task-btn bg-blue-700 rounded-full border-2 shadow-2xl border-white p-1"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Task;
